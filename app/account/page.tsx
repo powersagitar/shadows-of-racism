7bfb07688afb39bf40fe80be5313e020637af9de
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-23 12:12:12 -0400

    feat: add automatic role assignment based on gapps email

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -22,28 +22,31 @@ export default async function Account() {
   const [user] = (await selectUserByEmail(session.user!.email!)) as [User];
 
   if (!user) {
-    return (
-      <form
-        action={async (formData) => {
-          "use server";
-          const role = formData.get("role") as "teacher" | "artist";
-          await addUser({
-            name: session.user!.name!,
-            email: session.user!.email!,
-            role,
-            image: session.user!.image ?? undefined,
-          });
-          revalidatePath("/account");
-        }}
-      >
-        <p>select role</p>
-        <label>Teacher</label>
-        <input type="radio" name="role" value="teacher" />
-        <label>Artist</label>
-        <input type="radio" name="role" value="artist" defaultChecked />
-        <button type="submit">Submit</button>
-      </form>
-    );
+    try {
+      const role = getRole(session.user!.email!);
+
+      return (
+        <form
+          action={async () => {
+            "use server";
+            await addUser({
+              name: session.user!.name!,
+              email: session.user!.email!,
+              role,
+              image: session.user!.image ?? undefined,
+            });
+            revalidatePath("/account");
+          }}
+        >
+          Finish account creation Email: {session.user!.email!}
+          Role: {role}
+          <button type="submit">Create Account</button>
+        </form>
+      );
+    } catch (err) {
+      console.warn(err);
+      return <p>site for yrdsb students and teachers only</p>;
+    }
   }
 
   return (
@@ -61,3 +64,16 @@ export default async function Account() {
     </>
   );
 }
+
+const getRole = (email: string): User["role"] => {
+  const artistEmail = /^[0-9]{9}@gapps\.yrdsb\.ca$/g;
+  const teacherEmail = /^\w+\.\w+@gapps\.yrdsb\.ca$/g;
+
+  if (artistEmail.test(email)) {
+    return "artist";
+  } else if (teacherEmail.test(email)) {
+    return "teacher";
+  } else {
+    throw new Error("Invalid email");
+  }
+};
