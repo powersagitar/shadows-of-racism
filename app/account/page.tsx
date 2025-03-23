6c24d920947fe6f6551ef881d709dba78cb9cbd6
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-23 01:51:36 -0400

    refactor: update account creation logic

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,8 +1,13 @@
-import { signIn, signOut } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
+import { addUser, selectUserByEmail } from "@/lib/db/user";
+import { User } from "@/lib/types/user";
+import { revalidatePath } from "next/cache";
 
-export default function Account() {
-  return (
-    <>
+export default async function Account() {
+  const session = await auth();
+
+  if (!session) {
+    return (
       <form
         action={async () => {
           "use server";
@@ -11,7 +16,40 @@ export default function Account() {
       >
         <button type="submit">Signin with Google</button>
       </form>
+    );
+  }
 
+  const [user] = (await selectUserByEmail(session.user!.email!)) as [User];
+
+  if (!user) {
+    return (
+      <form
+        action={async (formData) => {
+          "use server";
+          const role = formData.get("role") as "teacher" | "artist";
+          await addUser({
+            name: session.user!.name!,
+            email: session.user!.email!,
+            role,
+            image: session.user!.image ?? undefined,
+          });
+          revalidatePath("/account");
+        }}
+      >
+        <p>select role</p>
+        <label>Teacher</label>
+        <input type="radio" name="role" value="teacher" />
+        <label>Artist</label>
+        <input type="radio" name="role" value="artist" defaultChecked />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+
+  return (
+    <>
+      <p>role: {user.role}</p>
+      <img src={user.image} alt={user.name} />
       <form
         action={async () => {
           "use server";
