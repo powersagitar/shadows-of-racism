63689cb18f02890f937c5e3b44619f29db694b7d
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-23 01:47:19 -0400

    refactor: update `users` schema

--- auth.ts
+++ auth.ts
@@ -1,15 +1,6 @@
 import NextAuth from "next-auth";
-import NeonAdapter from "@auth/neon-adapter";
-import { Pool } from "@neondatabase/serverless";
 import Google from "next-auth/providers/google";
 
-// *DO NOT* create a `Pool` here, outside the request handler.
-
-export const { handlers, auth, signIn, signOut } = NextAuth(() => {
-  // Create a `Pool` inside the request handler.
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  return {
-    adapter: NeonAdapter(pool),
-    providers: [Google],
-  };
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
 });
--- sql/schemas.sql
+++ sql/schemas.sql
@@ -1,46 +1,11 @@
-CREATE TABLE verification_token
-(
-  identifier TEXT NOT NULL,
-  expires TIMESTAMPTZ NOT NULL,
-  token TEXT NOT NULL,
-
-  PRIMARY KEY (identifier, token)
-);
-
-CREATE TABLE accounts
-(
-  id SERIAL,
-  "userId" INTEGER NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  provider VARCHAR(255) NOT NULL,
-  "providerAccountId" VARCHAR(255) NOT NULL,
-  refresh_token TEXT,
-  access_token TEXT,
-  expires_at BIGINT,
-  id_token TEXT,
-  scope TEXT,
-  session_state TEXT,
-  token_type TEXT,
-
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE sessions
-(
-  id SERIAL,
-  "userId" INTEGER NOT NULL,
-  expires TIMESTAMPTZ NOT NULL,
-  "sessionToken" VARCHAR(255) NOT NULL,
-
-  PRIMARY KEY (id)
-);
+CREATE TYPE ROLE AS ENUM ('artist', 'teacher');
 
 CREATE TABLE users
 (
-  id SERIAL,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  "emailVerified" TIMESTAMPTZ,
+  id SERIAL NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  role ROLE NOT NULL,
   image TEXT,
 
   PRIMARY KEY (id)
