ad2933c6a7898816af2b4a6dfbad44f275dd9c39
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-22 22:47:03 -0400

    feat: automatically redirect to `/account` if user is unauthenticated and on `/artist` or `/teacher`

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextRequest } from "next/server";
+
+const protectedRoutes = ["/artist", "/teacher"];
+
+export default async function middleware(req: NextRequest) {
+  const session = await auth();
+
+  if (!session && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const newUrl = new URL("/account", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
