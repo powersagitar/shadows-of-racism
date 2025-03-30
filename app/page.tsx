804ad9a87cae95435f0e8f25f1e2318948bc603e
ethan <18765895345lb@gmail.com>
2025-03-30 12:19:52 -0400

    Placeholder image

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <div className="font-inter flex flex-row gap-10 justify-between">
             <div className="w-1/2">
                 {/* Placeholder image */}
-                <img src="https://picsum.photos/1080/1080" />
+                <img src="https://picsum.photos/1080/1080" alt="placeholder image" />
             </div>
             <div className="flex flex-col gap-2 w-1/2 justify-end">
                 <Logo size="70px" />
