fcbd5ccebc4a6cfc277e3e1d800e3d8f14860c17
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-03-30 19:16:40 -0400

    chore: use 2 spaces for indentation

--- components/ui/socialmedia.tsx
+++ components/ui/socialmedia.tsx
@@ -4,37 +4,37 @@ import LinkedinIcon from "../icons/linkedin";
 import YoutubeIcon from "../icons/youtube";
 
 type SocialMediaProps = {
-    platform: "facebook" | "instagram" | "linkedin" | "youtube";
-    className?: string;
+  platform: "facebook" | "instagram" | "linkedin" | "youtube";
+  className?: string;
 };
 
 const links = {
-    instagram: "",
-    facebook: "",
-    linkedin: "",
-    youtube: "",
+  instagram: "",
+  facebook: "",
+  linkedin: "",
+  youtube: "",
 };
 
 export default function SocialMedia({ platform, className }: SocialMediaProps) {
-    const Icon = ({ className }: { className: string }) => {
-        switch (platform) {
-            case "facebook":
-                return <FacebookIcon className={className} />;
-            case "instagram":
-                return <InstagramIcon className={className} />;
-            case "linkedin":
-                return <LinkedinIcon className={className} />;
-            case "youtube":
-                return <YoutubeIcon className={className} />;
-        }
-    };
+  const Icon = ({ className }: { className: string }) => {
+    switch (platform) {
+      case "facebook":
+        return <FacebookIcon className={className} />;
+      case "instagram":
+        return <InstagramIcon className={className} />;
+      case "linkedin":
+        return <LinkedinIcon className={className} />;
+      case "youtube":
+        return <YoutubeIcon className={className} />;
+    }
+  };
 
-    return (
-        <a
-            className={`items-center justify-center ${className}`}
-            href={links[platform]}
-        >
-            <Icon className="h-full" />
-        </a>
-    );
+  return (
+    <a
+      className={`items-center justify-center ${className}`}
+      href={links[platform]}
+    >
+      <Icon className="h-full" />
+    </a>
+  );
 }
