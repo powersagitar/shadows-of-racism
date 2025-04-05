791cfb7adb66a6ccc351cbe1409f1edbfcfedda5
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-05 19:24:00 -0400

    feat: allow next/image to load images from vercel blob

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "my-blob-store.public.blob.vercel-storage.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
