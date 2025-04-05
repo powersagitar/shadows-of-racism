47c60eff60a7aa4ba3dffd5dd979517d6488f57e
chara@Metal-N-Magic <18765895345lb@gmail.com>
2025-04-05 11:18:24 -0400

    Dashboard empty page fix

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+             <iframe width="420" height="315"
+            src="https://www.youtube.com/embed/xvFZjo5PgG0">
+            </iframe> 
+    )
+}
