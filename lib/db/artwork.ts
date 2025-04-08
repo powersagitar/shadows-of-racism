ca3c35313dce0c2458498c46dd8db8c9c4102fcf
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-04-07 22:11:35 -0400

    refactor: cleanup the code a bit

--- lib/db/artwork.ts
+++ lib/db/artwork.ts
@@ -6,20 +6,7 @@ import ws from "ws";
 import { Artwork, ArtworkWithoutId } from "../types/artwork";
 neonConfig.webSocketConstructor = ws;
 
-export const insertArtwork = async ({
-  artwork_url,
-  uploader_id,
-  artist_name,
-  title,
-  medium,
-  width,
-  height,
-  depth,
-  school,
-  creation_date,
-  description,
-  description_recording_url,
-}: ArtworkWithoutId) => {
+export const insertArtwork = async (artwork: ArtworkWithoutId) => {
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   pool.on("error", (err) => console.error(err));
 
@@ -33,18 +20,18 @@ export const insertArtwork = async ({
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
       `,
       [
-        artwork_url.toString(),
-        uploader_id,
-        artist_name,
-        title,
-        medium,
-        width,
-        height,
-        depth,
-        school,
-        creation_date.toISOString(),
-        description,
-        description_recording_url.toString(),
+        artwork.artwork_url.toString(),
+        artwork.uploader_id,
+        artwork.artist_name,
+        artwork.title,
+        artwork.medium,
+        artwork.width,
+        artwork.height,
+        artwork.depth,
+        artwork.school,
+        artwork.creation_date.toISOString(),
+        artwork.description,
+        artwork.description_recording_url.toString(),
       ],
     );
     await client.query("COMMIT");
